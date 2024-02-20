9d26e04f1152bce84a35801158d8001615d4133c
“Rabia <rabia.a.ghafoor@gmail.com>
2024-02-20 13:58:05 -0500

    worked on adding classes

---
+++ src/main/java/edu/iu/habahram/coffeeorder/model/Decaf.java
@@ -0,0 +1,16 @@
+package edu.iu.habahram.coffeeorder.model;
+
+public class Decaf extends Beverage{
+
+    @Override
+    public String getDescription() {
+        return "Decaf";
+    }
+    @Override
+    public float cost() {
+        return 1.99F;
+    }
+
+
+}
+
