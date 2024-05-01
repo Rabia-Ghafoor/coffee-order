d63af82acaecedcf6a5cdfef19d0509832e7be5d
“Rabia <rabia.a.ghafoor@gmail.com>
2024-05-01 11:36:06 -0400

    added receipt

--- src/main/java/edu/iu/habahram/coffeeorder/repository/ReceiptRepository.java
+++ src/main/java/edu/iu/habahram/coffeeorder/repository/ReceiptRepository.java
@@ -1,13 +1,15 @@
 package edu.iu.habahram.coffeeorder.repository;
 
+import edu.iu.habahram.coffeeorder.model.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-public class ReceiptRepository {
-
     @Repository
-    public interface ReceiptRepository extends JpaRepository<Receipt, String> JpaSpecificationExecutor<Receipt> {
+    public interface ReceiptRepository extends JpaRepository<Receipt, String>, JpaSpecificationExecutor<Receipt> {
 
     }
 
 
-}
+
+
